2a59a8c1afbfa9daa9f747b67e891800ae4300cd
mutaGt <104976989+mutaGt@users.noreply.github.com>
2022-10-20 14:48:23 +0900

    viewファイル作成とルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'users/show'
   get 'users/edit'
+  get 'users/index'
   root to: 'homes#top'
+  resources :books
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
