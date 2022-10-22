65562c7e4b1c3c1e98a436ce16f1c4a461a5db6a
mutaGt <104976989+mutaGt@users.noreply.github.com>
2022-10-22 19:14:39 +0900

    ルーティングの編集とログイン後の遷移先

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'users' => 'users#index'
-  get 'homes/about'
   root to: 'homes#top'
-  resources :books
+  get 'homes/about'
   devise_for :users
+  resources :users
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
