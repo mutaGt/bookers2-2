8bf00a2dbefa0572ebdf5f23b8f4a30cdf66a553
mutaGt <104976989+mutaGt@users.noreply.github.com>
2022-10-31 15:07:55 +0900

    ユーザー一覧ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  
+  def index
+    @user = current_user
+    @users = User.all
+    @book = Book.new
+  end
+  
   def show
     @user = User.find(params[:id])
     @book = Book.new
