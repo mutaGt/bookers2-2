c865bf1dd61fa6321f4e9e18c7997bac094e7daf
mutaGt <104976989+mutaGt@users.noreply.github.com>
2022-11-03 15:42:43 +0900

    本の編集ページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,6 +17,16 @@ class BooksController < ApplicationController
     @user = @hon.user
   end
   
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
+  end
+  
   def destroy
     @hon = Book.find(params[:id])
     @hon.destroy
