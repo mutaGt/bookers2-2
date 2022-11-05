a7e1aee5abb74cdf2fe4c1b410d5335049a8f96f
mutaGt <104976989+mutaGt@users.noreply.github.com>
2022-11-05 14:50:06 +0900

    本の一覧ページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,12 @@ class BooksController < ApplicationController
     @book = Book.new
   end
   
+  def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+  end
+  
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
