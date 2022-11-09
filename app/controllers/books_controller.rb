89b44a8ba85231b995f1e0dbe0d82f4b831039eb
mutaGt <104976989+mutaGt@users.noreply.github.com>
2022-11-09 15:21:12 +0900

    フラッシュメッセージ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,6 +15,7 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     # if @book.save
     @book.save
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book)
     # else
     #   @books = Book.all
@@ -36,6 +37,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
     redirect_to book_path(@book)
   end
   
